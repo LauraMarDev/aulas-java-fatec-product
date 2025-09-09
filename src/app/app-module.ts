91917e449058d5a31f8038f1ffef87aab1a362c2
Laura Marques <lauraosmor@gmail.com>
2025-09-09 12:29:29 -0300

    fourth commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -5,11 +5,20 @@ import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductComponent } from './product-component/product-component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HomeComponent } from './home-component/home-component';
+import { FooterComponent } from './footer-component/footer-component';
+import { NavBarComponent } from './nav-bar-component/nav-bar-component';
+import { ClientComponent } from './client-component/client-component';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     App,
     ProductComponent,
+    HomeComponent,
+    FooterComponent,
+    NavBarComponent,
+    ClientComponent,
   ],
   imports: [
     BrowserModule,
@@ -18,7 +27,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient()
   ],
   bootstrap: [App]
 })
